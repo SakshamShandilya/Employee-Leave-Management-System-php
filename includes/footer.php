298651709285b12f3e19f2abad8fa942a11d81bc
saksham140023 <saksham140023@gmail.com>
2022-09-20 16:01:09 +0530

    new project

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Employee Leave Management System in PHP | Developed By TANMAY DUBEY</a></p>
+        </div>
+</footer>
